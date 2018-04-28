181b636cd85d3ee0259b7f8ff22d2556b3f1c344
odelfyette <odelfyette@gmail.com>
2018-04-28 08:44:53 -0400

    Added more samply data and angular directive

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,20 +1,40 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, EventEmitter, Output } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
     template: `<div class="well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-    <div>Date: {{event.date}} </div>
-    <div>Time: {{event.time}} </div>
-    <div>Price: \${{event.price}} </div>
-    <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+    <h2>{{event?.name}}</h2>
+    <div>Date: {{event?.date}} </div>
+    <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+    <div>Price: \${{event?.price}} </div>
+    <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{evennt?.location?.address}}
+        {{event?.location?.country}}</span>
     </div>
-    </div>`
+    <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+    </div>
+    <button class="btn btn-primary">Click Me!</button>
+    </div>`,
+    styles: [`
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent{
     @Input() event:any
+
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        else
+            return []
+    }
 }
